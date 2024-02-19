125f1f99253ccf40102ecb7a2c85df520d5e950f
Chungeun Choi <ce.choi@okestro.com>
2024-02-19 15:31:18 +0900

    fix - Add configuration to set the number of instances to 20.

--- openstack/compute/v2/servers/doc.go
+++ openstack/compute/v2/servers/doc.go
@@ -1,9 +1,6 @@
 /*
 Package servers provides information and interaction with the server API
 resource in the OpenStack Compute service.
-
-just test
-
 A server is a virtual machine instance in the compute system. In order for
 one to be provisioned, a valid flavor and image are required.
 
